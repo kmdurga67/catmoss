d50a94fb5d5db2262a153541e53e5dd5e4bc38b0
Kumari Durga <singhdurga5698@gmail.com>
2025-08-08 11:36:18 +0530

    created the publication page and creating the database for it, and displaying the database year wise latest data on top along with the publication

--- src/constants/publicationConstants.js
+++ src/constants/publicationConstants.js
@@ -1,247 +1,246 @@
 export const publicationConstant = [
-    {
-  "id": "pub_001",
-  "type": "journal_article",
-  "title": "Atmospheric-Pressure Continuous-Flow Methane Oxidation to Methanol and Acetic Acid Using H2O2 over the Au–Fe Catalyst",
-  "authors": [
-    "Anuradha V Jagtap", 
-    "Pawan Kumar", 
-    "Sharad Gupta",
-    "Abharana Nagendra",
-    "Shambhu Nath Jha",
-    "Dibyendu Bhattacharyya",
-    "Thalasseril G Ajithkumar",
-    "CP Vinod"
-  ],
-  "journal": "ACS Sustainable Chemistry & Engineering",
-  "year": 2024,
-  "volume": "12",
-  "issue": "23",
-  "pages": "8958-8967",
-  "doi": "10.1021/acssuschemeng.4c02993",
-  "impact_factor": 9.224,
-  "abstract": "Direct conversion of methane to methanol under mild conditions...",
-  "keywords": ["methane oxidation", "gold catalysts", "sustainable chemistry"],
-  "reaction": "CH4 + H2O2 → CH3OH + H2O",
-  "catalysts": ["Au-Fe"],
-  "conditions": {
-    "pressure": "1 atm",
-    "temperature": "80°C",
-    "flow_rate": "continuous"
-  },
-  "pdf_url": "/pubs/jagtap_2024_acs.pdf",
-  "funding": "SERB, India (CRG/2022/001234)"
-},
   {
-    "id": "pub_002",
-    "title": "Resorcinol–Azodianiline Covalent Organic Framework Supported FeOOH Quantum Dot-Catalyzed Electrochemical Ammonia Synthesis under Ambient Conditions",
-    "authors": [
-      "Pragalbh Shekhar", "Kento Kosugi", "Himan Dev Singh", 
-      "Rinku Kushwaha", "Deepak Rase", "Takumi Matsuzaki", 
-      "Chitvan Jain", "Piyush Singh", "Yashraj Singh", 
-      "C P Vinod", "Mio Kondo", "Shigeyuki Masaoka", 
-      "Ramanathan Vaidhyanathan"
+    "id":"pub_001",
+    "title": "Diverse reactivity patterns of nickel deposited on gold and tungsten surfaces",
+    "authors":[
+      " G. U. Kulkarni",
+      "C. P. Vinod"
     ],
-    "journal": "Chemistry of Materials",
-    "year": 2024,
-    "doi": "10.1021/acs.chemmater.4c00859",
-    "keywords": ["COF", "electrocatalysis", "ammonia synthesis"],
-    "catalysts": ["FeOOH quantum dots"]
-  },
-  {
-    "id": "pub_003",
-    "title": "Hydrogenation of Glucose to Sorbitol by Using Nickel Hydroxyapatite Catalyst",
-    "authors": ["PA Kamble", "CP Vinod", "VK Rathod", "M Lakshmi Kantam"],
-    "journal": "ChemCatChem",
-    "year": 2024,
-    "volume": "16",
-    "issue": "9",
-    "pages": "e202301590",
-    "doi": "10.1002/cctc.202301590",
-    "reaction": "C₆H₁₂O₆ + H₂ → C₆H₁₄O₆",
-    "catalysts": ["Ni-hydroxyapatite"]
-  },
-  {
-    "id": "pub_004",
-    "title": "Ligand Isomerization Driven Electrocatalytic Switching",
-    "authors": [
-      "Alagar Raja Kottaichamy", "Mohammed Azeezulla Nazrulla", 
-      "Muskan Parmar", "Ravikumar Thimmappa", 
-      "Mruthyunjayachari Chattanahalli Devendrachari", 
-      "Chathakudath Prabhakaran Vinod", "Michael Volokh", 
-      "Harish Makri Nimbegondi Kotresh", "Menny Shalom", 
-      "Musthafa Ottakam Thotiyl"
+    "other_details":"Appl. Surf. Sci., 115, 336 (1997).",
+    year:"1997"
+  },{
+    "id":"pub_002",
+    "title":"Size dependent changes on the electronic structure of metal clusters as investigated by Scanning tunneling spectroscopy",
+    authors: [
+      "C. P. Vinod",
+      "G. U. Kulkarni",
+      "C. N. R. Rao"
     ],
-    "journal": "Angewandte Chemie",
-    "year": 2024,
-    "doi": "10.1002/ange.202405664",
-    "keywords": ["electrocatalysis", "ligand isomerization"]
+    other_details:"Chem.Phys. Lett., 289, 329 (1998).",
+    year:"1998"
   },
   {
-    "id": "pub_005",
-    "title": "Aluminium, Nitrogen-Dual-Doped Reduced Graphene Oxide Co-Existing with Cobalt-Encapsulated Graphitic Carbon Nanotube as an Activity Modulated Electrocatalyst for Oxygen Electrochemistry Applications",
-    "authors": [
-      "Geeta Pandurang Kharabe", "Sidharth Barik", 
-      "Sudheesh Kumar Veeranmaril", "Aathira Nair", 
-      "Rajith Illathvalappil", "Athira Yoyakki", 
-      "Kavita Joshi", "Chathakudath Prabhakaran Vinod", 
-      "Sreekumar Kurungot"
+    id:"pub_003",
+    title:"Facile CO bond scission in alcohols on Zn surfaces",
+    authors: [
+      "K. R. Hari Kumar",
+      "C. P. Vinod",
+      "G. U. Kulkarni",
+      "C. N. R. Rao"
     ],
-    "journal": "Small",
-    "year": 2024,
-    "doi": "10.1002/smll.202400012",
-    "materials": ["Al/N-doped rGO", "Co@graphitic CNT"]
+    other_details:"J. Phys. Chem.B., 103, 2445 (1999).",
+    year:'1999'
   },
   {
-    "id": "pub_006",
-    "title": "Photocatalytic Regeneration of Reactive Cofactors with InP Quantum Dots for the Continuous Chemical Synthesis",
-    "authors": [
-      "IN Chakraborty", "V Jain", "P Roy", "P Kumar", 
-      "CP Vinod", "PP Pillai"
+    id: "pub_004",
+    title:"Formation of aligned carbon and carbon-nitrogen nanotubes on silica-supported iron and cobalt catalysts",
+    authors:[
+      "Manashi Nath",
+      "B. C. Satish Kumar",
+      "A. Govindraj",
+      "C. P. Vinod",
+      "C. N. R. Rao"
     ],
-    "journal": "ACS Catalysis",
-    "year": 2024,
-    "volume": "14",
-    "issue": "9",
-    "pages": "6740-6748",
-    "doi": "10.1021/acscatal.4c00817",
-    "nanomaterials": ["InP quantum dots"]
-  },
-  {
-    "id": "pub_007",
-    "title": "Unraveling the Cooperative Mechanisms in Ultralow Copper-Loaded WC@NGC for Enhanced CO2 Electroreduction to Acetic Acid",
-    "authors": [
-      "Debabrata Bagchi", "Mohd Riyaz", "Jithu Raj", 
-      "Soumyabrata Roy", "Ashutosh Kumar Singh", 
-      "Arjun Cherevotan", "Chathakudath P Vinod", 
-      "Sebastian C Peter"
+    other_details:"Chem. Phys. Lett., 322, 333 (2000).",
+    year:"2000"
+  },{
+    id:"pub_005",
+    title:"Interaction of carbon monoxide with Cu-Pd and Cu-Ni bimetallic clusters",
+    authors: [
+      "C. P. Vinod",
+      "K. R. Hari Kumar",
+      "G. U. Kulkarni",
+      "C. N. R. Rao"
     ],
-    "journal": "Chemistry of Materials",
-    "year": 2024,
-    "volume": "36",
-    "issue": "7",
-    "pages": "3464-3476",
-    "doi": "10.1021/acs.chemmater.4c00405",
-    "reaction": "CO₂ + H₂O → CH₃COOH + O₂"
-  },
-  {
-    "id": "pub_008",
-    "title": "Hydrogenation of furfural to tetrahydrofurfuryl alcohol over nickel-supported on organoclay catalyst",
-    "authors": ["PA Kamble", "CP Vinod", "VK Rathod", "ML Kantam"],
-    "journal": "Applied Catalysis A: General",
-    "year": 2024,
-    "volume": "674",
-    "pages": "119621",
-    "doi": "10.1016/j.apcata.2024.119621",
-    "catalysts": ["Ni/organoclay"]
-  },
-  {
-    "id": "pub_009",
-    "title": "Kinetic Model of Hydrogenation of Glucose to Sorbitol on a Ni/Bentonite Catalyst",
-    "authors": ["H Pandya", "CP Vinod", "V Rathod", "ML Kantam"],
-    "journal": "Industrial & Engineering Chemistry Research",
-    "year": 2024,
-    "volume": "63",
-    "issue": "11",
-    "pages": "4771-4781",
-    "doi": "10.1021/acs.iecr.3c03808",
-    "kinetics": true
-  },
-  {
-    "id": "pub_010",
-    "title": "Fuel from waste: electrosynthesizing ammonia directly from agricultural digestate through ligand isomerization",
-    "authors": [
-      "Rahul Mahadeo Mendhe", "Ritwik Mondal", 
-      "Alagar Raja Kottaichamy", "Akshay Haridas", 
-      "Harish Makri Nimbegondi Kotresh", 
-      "Chathakudath Prabhakaran Vinod", 
-      "Ravikumar Thimmappa", 
-      "Musthafa Ottakam Thotiyl"
+    other_details:"Topics in Catalysis., 11/12, 293 (2000).",
+    year:"2000"
+  },{
+    id:"pub_006",
+    title:"A method employing STM for the estimation of relative changes in the work function of modified metal tips",
+    authors: [
+      "R. B. Sharma",
+      "C. P. Vinod",
+      "G. U. Kulkarni"
     ],
-    "journal": "Green Chemistry",
-    "year": 2024,
-    "volume": "26",
-    "pages": "6490-6500",
-    "doi": "10.1039/D4GC00740A",
-    "process": "waste-to-ammonia"
-  },
-  {
-    "id": "pub_011",
-    "title": "Facile synthesis of nanostructured Ni/NiO/N-doped graphene electrocatalysts for enhanced oxygen evolution reaction",
-    "authors": [
-      "R Madampadi", "AB Patel", "CP Vinod", 
-      "R Gupta", "D Jagadeesan"
+    other_details:" Bulletin of Material Science., 25, 247 (2002).",
+    year:"2002"
+  },{
+    id:"pub_007",
+    title:"Boron nitride nanotubes and nanowires",
+    authors: [
+      "F. L. Deepak",
+      "C. P. Vinod",
+      "K. Mukhopadhyay",
+      "A. Govindaraj",
+      "C. N. R. Rao"
     ],
-    "journal": "Nanoscale Advances",
-    "year": 2024,
-    "volume": "6",
-    "issue": "11",
-    "pages": "2813-2822",
-    "doi": "10.1039/D4NA00141A",
-    "application": "OER"
-  },
-  {
-    "id": "pub_012",
-    "title": "Iron-catalyzed (E)-selective hydrosilylation of alkynes: scope and mechanistic insights",
-    "authors": [
-      "A Sen", "T Tewari", "R Kumar", "CP Vinod", 
-      "H Sharma", "K Vanka", "SH Chikkali"
+    other_details:"Chem. Phys. Lett., 353, 345 (2002).",
+    year:"2002"
+  },{
+    id: "pub_008",
+    title:"Nanoscale catalysis by Gold, Surface Chemistry and Catalysis",
+    authors:[
+      "G. U. Kulkarni",
+      "C. P. Vinod",
+      "C. N. R. Rao"
     ],
-    "journal": "Catalysis Science & Technology",
-    "year": 2024,
-    "volume": "14",
-    "issue": "10",
-    "pages": "2752-2760",
-    "doi": "10.1039/D3CY01775C",
-    "selectivity": "E-selective"
-  },
-  {
-    "id": "pub_013",
-    "title": "Electrochemical energy storage in an organic supercapacitor via a non-electrochemical proton charge assembly",
-    "authors": [
-      "Sanchayita Mukhopadhyay", "Alagar Raja Kottaichamy", 
-      "Mruthyunjayachari Chattanahalli Devendrachari", 
-      "Rahul Mahadeo Mendhe", 
-      "Harish Makri Nimbegondi Kotresh", 
-      "Chathakudath Prabhakaran Vinod", 
-      "Musthafa Ottakam Thotiyl"
+    other_details:"(Eds. A. Carley, P. Davies, G. Hutchings and M. Spencer, Springer, 2002)., , 191 - 206 (2002).",
+    year:"2002"
+  },{
+    id:"pub_009",
+    title:"A methanol-water complex stabilized on a Zn(0001) surface",
+    authors:[
+      "S. Vijayalakhshmi",
+      "C. P. Vinod",
+      "G. U. Kulkarni"
     ],
-    "journal": "Chemical Science",
-    "year": 2024,
-    "volume": "15",
-    "pages": "1726-1735",
-    "doi": "10.1039/D3SC05639B",
-    "device": "supercapacitor"
-  },
-  {
-    "id": "pub_014",
-    "title": "Deciphering Particle Morphology Effects in Cinnamaldehyde Hydrogenation over Palladium Nanostructures",
-    "authors": [
-      "G Porwal", "H KV", "S Sreedhala", 
-      "TS Khan", "MA Haider", "CP Vinod"
+    other_details:"Surf. Rev. Lett., 10, 87 (2003).",
+    year:"2003"
+  },{
+    id:"pub_010",
+    title:"Polyethersulfone supported titanium complexes as ethylene polymerization catalysts",
+    authors:[
+      "V. Gupta",
+      "C. P. Vinod",
+      "G. U. Kulkarni",
+      "G. K. Lahiri",
+      "N. Maity",
+      "S. Bhaduri"
     ],
-    "journal": "ACS Engineering Au",
-    "year": 2023,
-    "volume": "3",
-    "issue": "6",
-    "pages": "477-487",
-    "doi": "10.1021/acsengineeringau.3c00017",
-    "morphology_study": true
-  },
-  {
-    "id": "pub_015",
-    "title": "Morphology-Dependent Catalysis by Co3O4 Nanostructures in Atmospheric Pressure Carbon Dioxide Hydrogenation",
-    "authors": [
-      "S Gupta", "P Jain", "D Jagadeesan", 
-      "CP Vinod"
+    other_details:" Current Science., 88 ( 7), 1162 (2005).",
+    year:"2005"
+  },{
+    id:"pub_011",
+    title:"Synthesis and Reactivity of Dimethyl Gold Complexes Supported on MgO: Characterization by Infrared and X-ray Absorption Spectroscopies",
+    authors:[
+      "Javier Guzman",
+      "Bruce G. Anderson",
+      "C. P. Vinod",
+      "Kanaparthi Ramesh",
+      "J. W. Niemantsverdriet",
+      "Bruce C. Gates"
     ],
-    "journal": "The Journal of Physical Chemistry C",
-    "year": 2023,
-    "volume": "127",
-    "issue": "27",
-    "pages": "13055-13064",
-    "doi": "10.1021/acs.jpcc.3c02857",
-    "reaction": "CO₂ + H₂ → hydrocarbons"
+    other_details:"Langmuir., 21(8), 3675 (2005).",
+    year:'2005'
+  },{
+    id:"pub_012",
+    title:"Interaction of small molecules with Au(310): Decomposition of NO",
+    authors: [
+      "C.P.Vinod",
+      "J.W. Niemantsverdriet",
+      "B. E. Nieuwenhuys"
+    ],
+    other_details:"Applied Catalysis A-General., 291 (1-2), 93 (2005).",
+    year:"2005"
+  },{
+    id:"pub_013",
+    title:"NH3 adsorption and decomposition on Ir(110), a combined TPD and high resolution “fast” XPS study",
+    authors:[
+      "C.J. Westrate",
+      "E. D. L. Rienks",
+      "J. W. Bakker",
+      "S. Lizzit",
+      "L. Petaccia",
+      "A. Baraldi",
+      "C. P. Vinod",
+      "B. E. Nieuwenhuys"
+    ],
+    other_details:"J. Chem. Phys., 122, 184705 (2005).",
+    year:'2005'
+  },{
+    id:"pub_014",
+    title:"Zeolite NaY-supported gold complexes prepared from Au(CH3)2(C5H7O2): reactivity with carbon monoxide",
+    authors:[
+      "Juan C. Fierro-Gonzalez",
+      "Bruce G. Anderson",
+      "Kanaparthi Ramesh",
+      "C. P. Vinod",
+      "J. W. Niemantsverdriet",
+      "Bruce C. Gates"
+    ],
+    other_details:"Cat. Lett., 101 (3–4), 265 (2005).",
+    year:'2005'
+  },{
+    id:"pub_015",
+    title:"The role of Oad in the decomposition of NH3 adsorbed on Ir(110): a combined TPD and high-energy resolution fast XPS study",
+    authors:[
+      "C. J. Weststrate",
+      "J. W. Bakker",
+      "E. D. L. Rienks",
+      "C. P. Vinod",
+      "S. Lizzit",
+      "L. Petaccia",
+      "A. Baraldi",
+      "B. E. Nieuwenhuys"
+    ],
+    other_details:"Phys. Chem. Chem. Phys., 7(13), 2629 (2005).",
+    year:'2005'
+  },{
+    id:"pub_016",
+    title:"Chaos, oscillations and the golden future of catalysis",
+    authors:[
+      "B.E. Nieuwenhuys",
+      "A.C. Gluhoi",
+      "E.D.L. Rienks",
+      "C.J. Weststrate",
+      "C.P. Vinod"
+    ],
+    other_details:"Catalysis Today., 100 (1-2), 49 (2005).",
+    year:'2005'
+  },{
+    id:"pub_017",
+    title:"Selective NH3 oxidation on (110) and (111) iridium surfaces",
+    authors:[
+      "Weststrate CJ",
+      "Bakker JW",
+      "Rienks EDL",
+      "Martinez JR",
+      "Vinod CP",
+      "Lizzit S",
+      'Petaccia L',
+      "Baraldi A",
+      "Nieuwenhuys BE"
+    ],
+    other_details:" Journal of Catalysis., 235 (1), 92 (2005).",
+    year:'2005'
+  },{
+    id:"pub_018",
+    title:"Decomposition of methanol on Au(310)",
+    authors:[
+      "C.P.Vinod*",
+      "J.W. Niemantsverdriet",
+      "B. E. Nieuwenhuys*"
+    ],
+    other_details:"Phys. Chem. Chem. Phys., 7(8), 1824 (2005).",
+    year:'2005'
+  },{
+    id:"pub_019",
+    title:"Tricarbonyls of low-coordinated Au(0) atoms in zeolite-supported gold nanoparticles: Evidence from infrared and X-ray absorption spectroscopies",
+    authors:[
+      "Anderson BG",
+      "Fierro-Gonzalez JC",
+      "Ramesh K",
+      "Vinod CP",
+      "Niemantsverdriet JW",
+      "Gates BC"
+    ],
+    other_details:"Langmuir., 22 (9), 4310 (2006).",
+    year:"2006"
+  },{
+    id:"pub_020",
+    title:"Synchrotron XPS and desorption study of the NO chemistry on a stepped Pt surface",
+    authors:[
+      "Weststrate CJ",
+      "Bakker JW",
+      "Rienks EDL",
+      "Vinod CP",
+      "Lizzit S",
+      "Petaccia L",
+      "Baraldi A",
+      "Nieuwenhuys BE"
+    ],
+    other_details:"Surface Science., 600 (10), 1991 (2006).",
+    year:'2006'
   }
 
 ];
--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -1,9 +1,63 @@
+import React from 'react';
+import { publicationConstant } from '../constants/publicationConstants';
+
 const Publications = () => {
-    return (
-        <div>
-            This is publication pages.
-        </div>
-    );
+  const getYearFromDetails = (details) => {
+    const yearMatch = details.match(/\((\d{4})\)/);
+    return yearMatch ? parseInt(yearMatch[1]) : 0;
+  };
+
+  const groupedPublications = publicationConstant.reduce((acc, publication) => {
+    const year = getYearFromDetails(publication.other_details);
+    if (!acc[year]) {
+      acc[year] = [];
+    }
+    acc[year].push(publication);
+    return acc;
+  }, {});
+
+  const sortedYears = Object.keys(groupedPublications).sort((a, b) => b - a);
+
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-center mb-8">Publications</h1>
+      
+      <div className="max-w-4xl mx-auto">
+        {sortedYears.map((year) => (
+          <div key={year} className="mb-10">
+            <h2 className="text-2xl font-semibold mb-4 pb-2 border-b border-gray-200">{year}</h2>
+            <div className="space-y-6">
+              {groupedPublications[year].map((pub) => (
+                <PublicationItem key={pub.id} publication={pub} />
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const PublicationItem = ({ publication }) => {
+  return (
+    <div className="bg-white rounded-lg shadow-sm p-6 hover:shadow-md transition-shadow duration-200">
+      <h3 className="text-lg font-medium text-gray-800 mb-2">{publication.title}</h3>
+      
+      <div className="mb-3">
+        {publication.authors.map((author, index) => (
+          <span 
+            key={index} 
+            className={`text-gray-700 ${index === 0 ? 'font-semibold' : ''}`}
+          >
+            {author}
+            {index < publication.authors.length - 1 ? ', ' : ''}
+          </span>
+        ))}
+      </div>
+      
+      <p className="text-gray-600 italic">{publication.other_details}</p>
+    </div>
+  );
 };
 
 export default Publications;
